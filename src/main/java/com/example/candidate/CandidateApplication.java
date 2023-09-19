fd1baa49b366d021091bca63922a630dca0104dd
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-09-19 10:39:52 +0100

    config

--- src/main/java/com/example/candidate/CandidateApplication.java
+++ src/main/java/com/example/candidate/CandidateApplication.java
@@ -1,7 +1,11 @@
 package com.example.candidate;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 
@@ -10,5 +14,13 @@ public class CandidateApplication {
     public static void main(String[] args) {
         SpringApplication.run(CandidateApplication.class, args);
     }
-
+    @Bean
+    ApplicationRunner init(CandidatRepository repository) {
+        return args -> {
+            Stream.of("Mariem", "Sarra", "Mohamed").forEach(nom -> {
+                repository.save(new Candidat(nom));
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
 }
