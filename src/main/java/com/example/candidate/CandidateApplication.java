fecdbe0c94ff8a1192dbd9d7f2fedb7678eb7355
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-10-02 09:17:27 +0100

    associate microservice with eureka

--- src/main/java/com/example/candidate/CandidateApplication.java
+++ src/main/java/com/example/candidate/CandidateApplication.java
@@ -3,12 +3,13 @@ package com.example.candidate;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class CandidateApplication {
 
     public static void main(String[] args) {
